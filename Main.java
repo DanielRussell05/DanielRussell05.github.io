8b729f4b40e60312f0432e4756f7979b347e94ab
DanielRussell05 <122927926+DanielRussell05@users.noreply.github.com>
2023-03-23 17:49:55 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world, my name is Daniel Russell and I am 17 years old.");
+        System.out.println("My father's name is Charles");
+        System.out.println("My mother's name is Laura");
+        System.out.println("I have a sibling named Sarah");
+    }
+}
